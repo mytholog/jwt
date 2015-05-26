04cbbe8ba52e077c6e0dfc468e78a6863847f7fe
Igor Gavrilov <mytholog@yandex.ru>
2015-05-26 22:50:08 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+/**
+ * Created by mytholog on 26.05.15.
+ */
+var express = require('express');
+var faker = require('faker');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
+
+var jwtSecret = 'dhsjhsjdhsjdh72656253/4Gsh';
+
+var user = {
+    username: 'igor',
+    password: '123'
+};
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(expressJwt({ secret: jwtSecret }).unless({ path: ['/login'] }));
+
+app.get('/random-user', function(req, res) {
+    var user = faker.helpers.userCard();
+    user.avatar = faker.image.avatar();
+    res.json(user);
+});
+
+app.get('/me', function(req, res) {
+    res.send(req.user);
+});
+
+app.post('/login', authenticate, function(req, res) {
+    var token = jwt.sign({
+        username: user.username
+    }, jwtSecret);
+
+    res.send({
+        token: token,
+        user: user
+    });
+});
+
+app.listen(3000, function() {
+   console.log('App listening on localhost:3000');
+});
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next) {
+    var body = req.body;
+    if (!body.username || !body.password) {
+        res.status(400).end('Must provide usernameand password');
+    }
+
+    if (body.username !== user.username || body.password !== user.password) {
+        res.status(401).end('Username or password incorrect');
+    }
+
+    next();
+}
